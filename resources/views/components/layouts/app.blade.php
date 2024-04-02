93178280363d2695e61360cbf885232c7e51eda3
Idris <midexigner@gmail.com>
2024-04-02 09:03:05 +0500

    logout functionlity add and logo added

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="light">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +8,18 @@
         @vite('resources/css/app.css')
         @livewireStyles
     </head>
-    <body>
+    <body x-data="{
+    theme: localStorage.getItem('theme') ?? 'light',
+    changeTheme() {
+        this.theme = this.theme === 'dark' ? 'light' : 'dark'
+        localStorage.setItem('theme', this.theme)
+        document.querySelector('html').setAttribute('data-theme', this.theme);
+    },
+    init() {
+        document.querySelector('html').setAttribute('data-theme', this.theme);
+    }
+}">
+        <x-common.navbar/>
         {{ $slot }}
 
         @livewireScripts
